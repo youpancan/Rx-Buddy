9bea79d041bce1a2d4b63ff97520555d5da5ad6c
YOU PAN <sunnypanyou@gmail.com>
2023-03-07 16:56:21 -0500

    finish adding the view to profiles

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,7 @@ class ProfilesController < ApplicationController
 
   def dashboard
     # @user = current_user
-    @user_pharamcy_location = current_user.pharmacy_location
+    # @user_pharmacy_location = current_user.pharmacy_location
     current_user.user_medications.each do |medication|
       @active_meds << medication if medication.number_refills.positive?
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :birth_date, presence: true
-  validates :address, presence: true
-  validates :phone_number, presence: true
+  # validates :first_name, presence: true
+  # validates :last_name, presence: true
+  # validates :birthdate, presence: true
+  # validates :address, presence: true
+  # validates :phone_number, presence: true
 
   has_many :user_medications
   has_many :orders
