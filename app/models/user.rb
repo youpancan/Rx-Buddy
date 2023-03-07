ed3acacb7b311430e6b3209c34329afe006aebcd
Maggie hum <hum.maggie@gmail.com>
2023-03-07 15:24:45 -0500

    fixed the associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :user_medications
+  has_many :user_medications, :orders
 end
