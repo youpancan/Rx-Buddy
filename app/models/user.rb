1cadba803689e72618df8b44fa427043eed80230
Adham Al Koukou <adhamalkoukou@gmail.com>
2023-03-07 16:41:32 -0500

    added seeds and fixed some associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :birth_date, presence: true
-  validates :address, presence: true
-  validates :phone_number, presence: true
+  # validates :birth_date, presence: true
+  # validates :address, presence: true
+  # validates :phone_number, presence: true
 
   has_many :user_medications
   has_many :orders
