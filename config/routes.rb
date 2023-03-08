3c2790eab9ecfb7bb5390756a33129e6f054eda5
CloneProtocolSixtySix@users.noreply.github.com <yannick.bruderlein@icloud.com>
2023-03-08 14:22:43 -0500

    added edit action

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   get '/my_medications', to: 'user_medications#my_medications'
+  get '/dashboard/edit', to: 'porfiles#edit'
   resources :order, only: %i[new create show]
 end
