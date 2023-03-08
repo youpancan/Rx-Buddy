43f82be31e94e423c3d4746e70add2ff52779286
YOU PAN <sunnypanyou@gmail.com>
2023-03-08 10:48:12 -0500

    not finish yet

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,10 +1,11 @@
 class ProfilesController < ApplicationController
 
   def dashboard
-    # @user = current_user
+    @user = current_user
     # @user_pharmacy_location = current_user.pharmacy_location
-    current_user.user_medications.each do |medication|
-      @active_meds << medication if medication.number_refills.positive?
+    # current_user.user_medications.each do |medication|
+    #   @active_meds << medication if medication.number_refills.positive?
+    # end
+
     end
-  end
 end
