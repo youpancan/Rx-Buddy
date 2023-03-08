35423db1277f146f7eb97528eade063b1765254c
YOU PAN <sunnypanyou@gmail.com>
2023-03-08 13:45:54 -0500

    display all the data needed on porfile page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,11 +1,12 @@
 class ProfilesController < ApplicationController
 
   def dashboard
+    @active_meds = []
     @user = current_user
     # @user_pharmacy_location = current_user.pharmacy_location
-    # current_user.user_medications.each do |medication|
-    #   @active_meds << medication if medication.number_refills.positive?
-    # end
+    current_user.user_medications.each do |medication|
+      @active_meds.push(medication) if medication.number_refills.positive?
+    end
 
     end
 end
