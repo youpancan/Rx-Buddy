0866b91b91732baeaa80622bc929807374720a5d
Maggie hum <hum.maggie@gmail.com>
2023-03-09 14:42:56 -0500

    did a few changes

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,7 @@
 class ProfilesController < ApplicationController
+  def show
+    @user = current_user
+  end
 
   def edit
     @user = current_user
@@ -9,7 +12,7 @@ class ProfilesController < ApplicationController
     @user.update(user_params)
 
     if @user.save
-      redirect_to dashboard_path
+      redirect_to dashboard_show_path
     else
       render :edit, status: unprocessable_entity
     end
@@ -22,13 +25,11 @@ class ProfilesController < ApplicationController
     current_user.user_medications.each do |medication|
       @active_meds.push(medication) if medication.number_refills.positive?
     end
+  end
 
-    end
+  private
 
-
-    private
-
-    def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :birthday, :phone_number, :address, )
-    end
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :birthday, :phone_number, :address)
+  end
 end
