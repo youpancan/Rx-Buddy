f4fa5089fe46681d1b284c97eb6362fd4c15fef5
Maggie hum <hum.maggie@gmail.com>
2023-03-14 10:56:21 -0400

    added the remove button to the allergy tom-select

--- app/javascript/controllers/tom_select_controller.js
+++ app/javascript/controllers/tom_select_controller.js
@@ -3,12 +3,19 @@ import TomSelect from "tom-select";
 
 // Connects to data-controller="tom-select"
 export default class extends Controller {
-  static values = { options: Object }
+  // static values = { options: Object }
 
   connect() {
     new TomSelect(
-      this.element ,
-      this.optionsValue
-    );
+      this.element, {
+          plugins: {
+            remove_button:{
+              title:'Remove this item',
+            }
+          },
+          persist: false,
+          create: true,
+          tags: true
+    });
   }
 }
