d6aae11a4c7a8ad175bc28bedaf50b55273d018f
Adham Al Koukou <adhamalkoukou@gmail.com>
2023-03-14 11:45:29 -0400

    enum methods in medication card

--- app/models/refill.rb
+++ app/models/refill.rb
@@ -4,7 +4,6 @@ class Refill < ApplicationRecord
   has_one :notification
   enum status: { pending: 0, received: 1, in_process: 2, complete: 3, picked_up: 4 }
   enum urgency: { not_urgent: 0, medium: 1, urgent: 2, not_specified: 3 }
-  # declare enum and change status & urgency to integer
-  # dashboard should show that a refill instance has been made
-  # maybe add status in order which depends on the refill status for all refills
+  # when we put picked_up!, the refill due date for medication should be 30 days from now
+  # and amount of refills should decrease by 1
 end
