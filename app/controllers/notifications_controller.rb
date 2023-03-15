18751590d3c59af009becdfcb57f7b933922e702
Maggie hum <hum.maggie@gmail.com>
2023-03-15 09:36:09 -0400

    Reenable notifications controller class

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,7 +1,8 @@
-# class NotificationsController < ApplicationController
-#   def show
-#     @user = current_user
-#     @sum = 0
-#     @order_meds = []
-#   end
-# end
+class NotificationsController < ApplicationController
+  # [TODO] Delete this class once we know it's no longer needed.
+  # def show
+  #   @user = current_user
+  #   @sum = 0
+  #   @order_meds = []
+  # end
+end
