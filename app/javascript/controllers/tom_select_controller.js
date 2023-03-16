5454ea721f64e35459cd573f41a8041119636b72
Maggie hum <hum.maggie@gmail.com>
2023-03-16 09:58:02 -0400

    fixed the allergy tom-select

--- app/javascript/controllers/tom_select_controller.js
+++ app/javascript/controllers/tom_select_controller.js
@@ -13,6 +13,9 @@ export default class extends Controller {
               title:'Remove this item',
             }
           },
+          onItemAdd:function(){
+            this.setTextboxValue('');
+          },
           persist: false,
           create: true,
           tags: true
